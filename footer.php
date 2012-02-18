14da135096b5ef7b135c56a41ef9a8ec77e6e0d4
Nathan Carnes <nathan@carnesmedia.com>
2012-02-17 18:22:57 -0800

    Styling footer

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
   <!-- Begin footer.php -->
   </section>                                                                                                                                                        
-  <footer class="main">
-    DAS FOOTER
+  <footer class="main">                             
+    <div class="copyright">
+      &copy; <?= date('Y'); ?> <? bloginfo('name'); ?>
+    </div>
+    <div class="social">
+      <a href="#/" class="facebook">Facebook</a>
+      <a href="#/" class="twitter">Twitter</a>
+      <a href="#/" class="youtube">YouTube</a>
+    </div>
   </footer>
 </body>                                                                                             
 </html>
