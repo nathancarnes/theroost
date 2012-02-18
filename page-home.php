83976ddc84604229742454dd8e091dc041ad2f9a
Nathan Carnes <nathan@carnesmedia.com>
2012-02-17 22:09:54 -0800

    Stubbing out homepage

---
+++ page-home.php
@@ -0,0 +1,52 @@
+<? get_header(); ?>
+  <p id="masthead">
+    We are an independently run<br>
+    music venue that showcases artists in the intimacy<br>
+    of a living room.  
+  </p>
+   
+  <!-- Start Slider -->         
+  <div id="slider">
+    <div class="slide">
+      <img src="http://dummyimage.com/920x360/333333/666666&text=PHOTO" alt="TITLE">
+      <div class="info">
+        <h4>TITLE</h4>       
+        <div class="date">Saturday, May 5</div>
+        <div class="time">8pm</div>
+        <a href="#/">RSVP</a>
+      </div>
+    </div>
+  </div>    
+  <div class="slider_nav">
+    <a href="#/" class="active">&middot;</a>
+    <a href="#/">&middot;</a>
+    <a href="#/">&middot;</a>
+  </div>          
+  <div class="clear"></div>
+  <!-- End Slider -->
+  
+  <!-- Start Featured --> 
+  <div id="featured">
+    <h3>Featured Artists</h3>
+    <div class="first featured_artist">
+      <img src="http://dummyimage.com/290x150/333333/666666&text=PHOTO" alt="TITLE">    
+      <h4>Artist Name</h4>                   
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p><a href="#/">website &gt;</a></p>
+    </div>                        
+    <div class="featured_artist">
+      <img src="http://dummyimage.com/290x150/333333/666666&text=PHOTO" alt="TITLE">    
+      <h4>Artist Name</h4>                   
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p><a href="#/">website &gt;</a></p>
+    </div>
+    <div class="last featured_artist">
+      <img src="http://dummyimage.com/290x150/333333/666666&text=PHOTO" alt="TITLE">    
+      <h4>Artist Name</h4>                   
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p><a href="#/">website &gt;</a></p>
+    </div>           
+  </div>
+  <!-- End Featured -->  
+
+<? get_footer(); ?>
