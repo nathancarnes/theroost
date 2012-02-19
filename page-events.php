78e2fe9702b7352ad455ccf7491cb6cef476c9c8
Nathan Carnes <nathan@carnesmedia.com>
2012-02-18 19:29:44 -0800

    Stubbing out events page

---
+++ page-events.php
@@ -0,0 +1,65 @@
+<? get_header(); ?>
+<div class="left_column featured_events">
+  <div class="featured_event">
+    <div class="date">Saturday, May 5</div>
+    <div class="time">8pm</div>
+    <h4>Artist Name</h4>       
+    <div class="cost">
+      All Ages $10
+    </div>               
+    <img src="http://dummyimage.com/480x360/333333/666666&text=PHOTO" alt="TITLE">
+    <a href="#/">RSVP</a>
+  </div>   
+  <div class="featured_event">
+    <div class="date">Saturday, May 5</div>
+    <div class="time">8pm</div>
+    <h4>Artist Name</h4>       
+    <div class="cost">
+      All Ages $10
+    </div>               
+    <img src="http://dummyimage.com/480x360/333333/666666&text=PHOTO" alt="TITLE">
+    <a href="#/">RSVP</a>
+  </div>      
+</div>                 
+
+<div class="right_column">
+  <h3>Upcoming Shows</h3>
+  <ol class="upcoming_shows">
+    <li class="month"><h6>July</h6></li>
+    <li>
+      <div class="artist">Artist name</div>
+      <div class="date">Saturday, May 5</div>
+      <div class="time">8pm</div>
+    </li>
+    <li>
+      <div class="artist">Artist name</div>
+      <div class="date">Saturday, May 5</div>
+      <div class="time">8pm</div>
+    </li>
+    <li>
+      <div class="artist">Artist name</div>
+      <div class="date">Saturday, May 5</div>
+      <div class="time">8pm</div>
+    </li>
+    <li class="month"><h6>August</h6></li>
+    <li>
+      <div class="artist">Artist name</div>
+      <div class="date">Saturday, May 5</div>
+      <div class="time">8pm</div>
+    </li>
+    <li>
+      <div class="artist">Artist name</div>
+      <div class="date">Saturday, May 5</div>
+      <div class="time">8pm</div>
+    </li>
+    <li>
+      <div class="artist">Artist name</div>
+      <div class="date">Saturday, May 5</div>
+      <div class="time">8pm</div>
+    </li>                  
+  </ol>
+</div>
+
+<div class="clear"></div>
+
+<? get_footer(); ?>
