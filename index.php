04e65b7d284d79333b62ef4576a5c710d4bd6d56
Nathan Carnes <nathan@carnesmedia.com>
2012-02-20 09:54:57 -0800

    Stubbing out index

--- index.php
+++ index.php
@@ -1,6 +1,55 @@
 <? get_header(); ?>
-  <h1>Heading 1</h1>
-  <h2>Heading 2</h2>
-  <h3>Heading 3</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>                        
+
+<div class="left_column">
+  <? /* If there are no posts to display, such as an empty archive page */ ?>
+  <? if ( ! have_posts() ) : ?>
+  		<h1>Nope nope nope</h1>
+  		<p>Ain't nuttin' here</p>
+  		<? get_search_form(); ?>
+  <? endif; ?>
+
+  <? while ( have_posts() ) : the_post(); ?>  
+    <article <? post_class(); ?> id="post-<? the_ID(); ?>">
+  	  <header>
+  	    <h2><a href="<? the_permalink(); ?>" title="Permalink to <? the_title(); ?>" rel="bookmark"><? the_title(); ?></a></h2>
+  	    <div class="date"><? the_date(); ?></div>  
+  	  </header>
+      <section class="excerpt">
+      	<? if ( is_archive() || is_search() ) : ?>
+      			<? the_excerpt(); ?>
+      	<? else : ?>
+      			<? the_content( 'Read more...' ); ?>
+      	<? endif; ?>      
+      </section>
+      <footer>
+        <ul>
+      	  <? if ( count( get_the_category() ) ) : ?>
+      	    <li class="categories">Filed under <?= get_the_category_list( ', ' ); ?></li>   
+      		<? endif; ?>
+      		<? if ( get_the_tag_list( '' ) ): ?>      
+      	    <li class="tags">Taggged <?= get_the_tag_list( '', ', ' ); ?></li>
+      		<? endif; ?>       
+      		<? if( comments_open() ):?>
+      	  <li class="comments">
+      	    <? comments_popup_link( 'No Comments', 'One Comment', '# Comments' ); ?>
+      	  </li>    
+      	  <? endif; ?>
+      	</ul>
+      </footer>
+    </article>
+  <? endwhile; ?>
+
+  <div class="pagination">
+    <? if ( $wp_query->max_num_pages > 1 ) : ?>
+        <? next_posts_link('&laquo; Older'); ?>
+        <? previous_posts_link('Newer &raquo;'); ?>
+    <? endif; ?>
+  </div>
+</div>
+
+<div class="right_column">
+  <? get_sidebar(); ?>
+</div>        
+<div class="clear"></div>
+
 <? get_footer(); ?>
--- page.php
+++ page.php
@@ -1,8 +1,18 @@
 <? get_header(); ?> 
   <h2><? the_title(); ?></h2>
+              
+  <div class="left_column">
+    <div class="content">
+      <? the_content(); ?>   
+      <p>Austin swag four loko cred, put a bird on it bushwick master cleanse fap odd future hoodie organic. Biodiesel polaroid keffiyeh flexitarian mlkshk selvage raw denim ennui gentrify chillwave. Bushwick pinterest terry richardson seitan master cleanse, mlkshk squid pitchfork iphone synth art party banksy lo-fi vice. Pickled keytar quinoa, mlkshk you probably haven't heard of them typewriter authentic portland wayfarers terry richardson PBR pitchfork. Keytar ennui blog, readymade cosby sweater polaroid semiotics twee skateboard high life photo booth organic mlkshk. Cliche PBR godard letterpress marfa. Typewriter cred cardigan, polaroid ethical pop-up etsy single-origin coffee narwhal viral trust fund.</p>
+      
+    </div>
+  </div>
   
-  <div class="content">
-    <? the_content(); ?>   
+  <div class="right_column">
+    <h3>Blah</h3>
+    <p>Austin swag four loko cred, put a bird on it bushwick master cleanse fap odd future hoodie organic. Biodiesel polaroid keffiyeh flexitarian mlkshk selvage raw denim ennui gentrify chillwave. Bushwick pinterest terry richardson seitan master cleanse, mlkshk squid pitchfork iphone synth art party banksy lo-fi vice. Pickled keytar quinoa, mlkshk you probably haven't heard of them typewriter authentic portland wayfarers terry richardson PBR pitchfork. Keytar ennui blog, readymade cosby sweater polaroid semiotics twee skateboard high life photo booth organic mlkshk. Cliche PBR godard letterpress marfa. Typewriter cred cardigan, polaroid ethical pop-up etsy single-origin coffee narwhal viral trust fund.</p>    
   </div>
 
+  <div class="clear"></div>
 <? get_footer(); ?>
