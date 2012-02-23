08236a13a1a7db0f544fd03543d069455966454e
Nathan Carnes <nathan@carnesmedia.com>
2012-02-22 17:10:04 -0800

    Data setup for homepage

--- page-home.php
+++ page-home.php
@@ -4,31 +4,20 @@
   </p>
    
   <!-- Start Slider -->         
-  <div id="slider">
-    <div class="slide" style="background-image: url(http://dummyimage.com/920x360/333333/666666&text=FIRST);">
-      <div class="info">
-        <h4>TITLE</h4>       
-        <div class="date">Saturday, May 5</div>
-        <div class="time">8pm</div>
-        <a href="#/">RSVP</a>
-      </div>         
-    </div> 
-    <div class="slide" style="background-image: url(http://dummyimage.com/920x360/333333/666666&text=SECOND);">
-      <div class="info">
-        <h4>TITLE</h4>       
-        <div class="date">Saturday, May 5</div>
-        <div class="time">8pm</div>
-        <a href="#/">RSVP</a>
-      </div>          
-    </div>
-    <div class="slide" style="background-image: url(http://dummyimage.com/920x360/333333/666666&text=THIRD);">
-      <div class="info">
-        <h4>TITLE</h4>       
-        <div class="date">Saturday, May 5</div>
-        <div class="time">8pm</div>
-        <a href="#/">RSVP</a>
-      </div>  
-    </div>        
+  <div id="slider">  
+    <? global $post; ?>  
+    <? foreach(get_field('featured_events') as $post): ?>  
+      <? setup_postdata($post); ?>
+      <div class="slide" style="background-image: url(<? the_field('event_banner_photo'); ?>);">
+        <div class="info">
+          <h4><? the_title(); ?></h4>       
+          <div class="date"><? the_field('event_date'); ?></div>
+          <div class="time"><? the_field('event_time'); ?></div>
+          <? if(get_field('event_rvsp_link')): ?><a href="<? the_field('event_rvsp_link'); ?>">RSVP</a><? endif; ?>
+        </div>         
+      </div>     
+    <? endforeach; ?>
+    <? wp_reset_postdata(); ?>
   </div>    
   <div class="slider_nav"></div>          
   <div class="clear"></div>
@@ -36,25 +25,21 @@
   
   <!-- Start Featured --> 
   <div id="featured">
-    <h3>Featured Artists</h3>
-    <div class="first featured_artist">
-      <img src="http://dummyimage.com/290x150/333333/666666&text=PHOTO" alt="TITLE">    
-      <h4>Artist Name</h4>                   
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p><a href="#/">website &gt;</a></p>
-    </div>                        
-    <div class="featured_artist">
-      <img src="http://dummyimage.com/290x150/333333/666666&text=PHOTO" alt="TITLE">    
-      <h4>Artist Name</h4>                   
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p><a href="#/">website &gt;</a></p>
-    </div>
-    <div class="last featured_artist">
-      <img src="http://dummyimage.com/290x150/333333/666666&text=PHOTO" alt="TITLE">    
-      <h4>Artist Name</h4>                   
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p><a href="#/">website &gt;</a></p>
-    </div>           
+    <h3>Featured Artists</h3>  
+    <? $positions = array('first', 'middle', 'last'); $i = 0; ?>         
+    <? foreach(get_field('featured_artists') as $post): ?>  
+      <? setup_postdata($post); ?>
+      <div class="<?= $positions[$i]; ?> featured_artist">
+        <img src="<? the_field('artist_small_photo'); ?>" alt="<? the_title(); ?>">    
+        <h4><? the_title(); ?></h4>                   
+        <p><? the_field('artist_blurb'); ?></p>
+        <? if(get_field('artist_website')): ?>
+          <p><a href="<? the_field('artist_website'); ?>">website &gt;</a></p>
+        <? endif; ?>
+      </div>    
+      <? $i++; ?>                          
+    <? endforeach; ?>
+    <? wp_reset_postdata(); ?>  
   </div>
   <!-- End Featured -->  
 
